1d9854f2482c7d3a9b8628d36f0ea06632ef6de7
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-10-16 18:11:15 -0700

    finished step 5

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import "./style.css";
 
 // Globals
-let numAnts: number = 0;
+let numPapers: number = 0;
 let timeSinceFrame: number = performance.now();
+let passivePaperGrowth: number = 0;
+let clickValue: number = 1;
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Ant Mountain";
+const gameName = "Lord of News";
 document.title = gameName;
 
 const header = document.createElement("h1");
@@ -14,26 +16,46 @@ header.innerHTML = gameName;
 app.append(header);
 
 // Create the button
-const button = document.createElement("button");
-button.innerHTML = "Ant 🐜";
-app.append(button);
+const manualClickButton = document.createElement("button");
+manualClickButton.innerHTML = "📰";
+app.append(manualClickButton);
 
 // Track button clicks
-const buttonClickCounter = document.createElement("div");
-buttonClickCounter.textContent = `${numAnts} Ants`;
-app.append(buttonClickCounter);
-button.addEventListener("click", () => {
-  numAnts += 1;
-  buttonClickCounter.textContent = `${numAnts} Ants`;
+const manualClickCounter = document.createElement("div");
+updateClick(0);
+app.append(manualClickCounter);
+manualClickButton.addEventListener("click", () => {
+  updateClick(clickValue);
 });
+requestAnimationFrame(continuousPaperGain);
 
-requestAnimationFrame(continuousAntGain);
-function continuousAntGain(timestamp: number) {
+// Add button to upgrade passive click gains
+const upgradePassivePaper = document.createElement("button");
+upgradePassivePaper.innerHTML = "Delivery Boy (Costs 10 Paper)";
+app.append(upgradePassivePaper);
+upgradePassivePaper.disabled = true;
+upgradePassivePaper.addEventListener("click", ()=>{
+    let increment: number = Math.floor(numPapers/10);
+    updateClick(0-(increment*10))
+    passivePaperGrowth += increment;
+})
+
+
+
+
+function continuousPaperGain(timestamp: number) {
   const deltaTime = timestamp - timeSinceFrame;
   if (deltaTime >= 1000) {
-    numAnts += 10;
-    buttonClickCounter.textContent = `${numAnts} Ants`;
+    updateClick(passivePaperGrowth);
     timeSinceFrame = timestamp;
   }
-  requestAnimationFrame(continuousAntGain);
+  requestAnimationFrame(continuousPaperGain);
+}
+
+function updateClick(change: number){
+    numPapers += change;
+    manualClickCounter.textContent = `${numPapers} Papers`;
+    if (numPapers >= 10){
+        upgradePassivePaper.disabled = false;
+    }
 }
