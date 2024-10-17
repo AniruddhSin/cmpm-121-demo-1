82a49aa674086018d5ed97ffa211b54c141f484f
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-10-16 17:07:00 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My totally amazing game";
+const gameName = "Ant Mountain";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Create the button
 const button = document.createElement("button");
 button.innerHTML = "Ant 🐜";
 app.append(button);
+
+// Track button clicks
+let numClicks: number = 0;
+const buttonClickCounter = document.createElement("div");
+buttonClickCounter.textContent = `${numClicks} Ants`;
+app.append(buttonClickCounter);
+button.addEventListener('click', ()=>{
+    numClicks+=1;
+    buttonClickCounter.textContent = `${numClicks} Ants`;
+})
