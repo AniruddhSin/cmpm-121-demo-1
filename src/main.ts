15ca7a0699b6a567abad032b619ee0cfaa507663
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-11-04 12:50:09 -0800

    D1.C changes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import "./style.css";
 // Globals
 let numPapers: number = 0;
 let timeSinceFrame: number = performance.now();
-let passivePaperGrowth: number = 0;
+let paperGrowthRate: number = 0;
 const clickValue: number = 1;
-const clickObservers: Upgrade[] = [];
+const availableItems: Upgrade[] = [];
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const status_display: HTMLDivElement =
@@ -17,6 +17,19 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Function to handle logic when newspapers value changes
+function updateClick(change: number) {
+  numPapers += change;
+  paperCounter.textContent = numPapers.toFixed(1) + " Papers";
+  for (const observer of availableItems) {
+    if (numPapers >= observer.getCost()) {
+      observer.enable();
+    } else {
+      observer.disable();
+    }
+  }
+}
 
 // Create the button
 const manualClickButton = document.createElement("button");
@@ -32,15 +45,15 @@ manualClickButton.addEventListener("click", () => {
 });
 
 // Start passive paper gain
-requestAnimationFrame(continuousPaperGain);
 function continuousPaperGain(timestamp: number) {
   const deltaTime = timestamp - timeSinceFrame;
   if (deltaTime >= 1000) {
-    updateClick(passivePaperGrowth);
+    updateClick(paperGrowthRate);
     timeSinceFrame = timestamp;
   }
   requestAnimationFrame(continuousPaperGain);
 }
+requestAnimationFrame(continuousPaperGain);
 
 // Create class to define Upgrades and track their status
 interface Item {
@@ -51,8 +64,9 @@ interface Item {
 }
 class Upgrade implements Item {
   counterDisplay: HTMLDivElement;
-  upgradeCounter: number;
+  purchaseCount: number;
   upgradeCost: number;
+  costMultiplier: number = 1.15;
   upgradeValue: number;
   itemName: string;
   button: HTMLButtonElement;
@@ -66,14 +80,14 @@ class Upgrade implements Item {
     descript: string,
   ) {
     this.itemName = label;
-    this.upgradeCounter = 0;
+    this.purchaseCount = 0;
     this.upgradeCost = cost;
     this.upgradeValue = value;
     this.description = descript;
 
     // Create and display upgrade counter
     this.counterDisplay = document.createElement("div");
-    this.counterDisplay.innerHTML = `${this.upgradeCounter} ${this.itemName}s`;
+    this.counterDisplay.innerHTML = `${this.purchaseCount} ${this.itemName}s`;
     status_display.append(this.counterDisplay);
 
     // Create interactable button
@@ -84,16 +98,16 @@ class Upgrade implements Item {
     this.button.addEventListener("click", () => {
       // keep attempting purchases until fail
       while (this.purchased()) {
-        this.upgradeCost *= 1.15;
-        this.upgradeCounter += 1;
+        this.upgradeCost *= this.costMultiplier;
+        this.purchaseCount += 1;
       }
       // update text after purchases were made
-      this.counterDisplay.innerHTML = `${this.upgradeCounter} ${this.itemName}s`;
+      this.counterDisplay.innerHTML = `${this.purchaseCount} ${this.itemName}s`;
       this.button.innerHTML = `${label}: ${this.upgradeCost.toFixed(1)} Papers`;
     });
 
     this.button.disabled = true;
-    clickObservers.push(this);
+    availableItems.push(this);
   }
 
   purchased() {
@@ -101,7 +115,7 @@ class Upgrade implements Item {
       return false;
     }
     updateClick(0 - this.upgradeCost);
-    passivePaperGrowth += this.upgradeValue;
+    paperGrowthRate += this.upgradeValue;
     return true;
   }
 
@@ -118,32 +132,39 @@ class Upgrade implements Item {
   }
 }
 
-/*// Add button to upgrade passive click gains
-const upgradePassivePaper = document.createElement("button");
-upgradePassivePaper.innerHTML = "Delivery Boy (Costs 10 Paper)";
-app.append(upgradePassivePaper);
-upgradePassivePaper.disabled = true;
-upgradePassivePaper.addEventListener("click", () => {
-  const increment: number = Math.floor(numPapers / 10);
-  updateClick(0 - increment * 10);
-  passivePaperGrowth += increment;
-});*/
-
-function updateClick(change: number) {
-  numPapers += change;
-  paperCounter.textContent = numPapers.toFixed(1) + " Papers";
-  for (const observer of clickObservers) {
-    if (numPapers >= observer.getCost()) {
-      observer.enable();
-    } else {
-      observer.disable();
-    }
-  }
-}
-
 // Add Upgrades (label, cost, value, button, title)
-new Upgrade("Delivery Boy", 1, 0.1, document.createElement("button"), "He earns less than minimum wage");
-new Upgrade("Amazon Truck", 10, 2, document.createElement("button"), "The driver gets 4 hours of sleep per night");
-new Upgrade("Printer", 100, 15, document.createElement("button"), "Spread your own misinformation");
-new Upgrade("Investor", 250, 50, document.createElement("button"), "They scheme using your misinformation");
-new Upgrade("Company", 1000, 100, document.createElement("button"), "Expand your empire of lies");
+new Upgrade(
+  "Delivery Boy",
+  1,
+  0.1,
+  document.createElement("button"),
+  "He earns less than minimum wage",
+);
+new Upgrade(
+  "Amazon Truck",
+  10,
+  2,
+  document.createElement("button"),
+  "The driver gets 4 hours of sleep per night",
+);
+new Upgrade(
+  "Printer",
+  100,
+  15,
+  document.createElement("button"),
+  "Spread your own misinformation",
+);
+new Upgrade(
+  "Investor",
+  250,
+  50,
+  document.createElement("button"),
+  "They scheme using your misinformation",
+);
+new Upgrade(
+  "Company",
+  1000,
+  100,
+  document.createElement("button"),
+  "Expand your empire of lies",
+);
